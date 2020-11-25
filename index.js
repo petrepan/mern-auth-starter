a9c934d6102779667b304fc3df5bc5cee7b8a011
petrepan <peterawotola@gmail.com>
2020-11-25 10:26:41 +0100

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const configDb = require("./config/db");
 const userRoute = require("./routes/user")
 
@@ -9,6 +10,7 @@ configDb();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
